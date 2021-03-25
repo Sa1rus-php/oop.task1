8fe8b6c9dcaf4adba5a1f90a5ba62d16509fd500
Dmitriy Rekhlitskii <drekhlitskii@gmail.com>
2021-03-25 17:24:24 +0200

    Delete hooks directory

--- hooks/post-merge.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
-check_run() {
-	echo "$changed_files" | grep --quiet "$1" && eval "$2"
-}
-cd sources
-#check_run package.json "npm prune && npm install"
